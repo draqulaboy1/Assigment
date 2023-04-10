a3aa6041b685bac9c567e03586e3f3922c5843b5
draqulaboy1 <himanshu.kumar@cawstudios.com>
2023-04-11 05:20:49 +0530

    final comit

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-   
+   env:{
+    json:[{"name" : "Bob", "age" : 20, "sex": "Male"},
+    {"name": "George", "age" : 42, "sex": "Male"},
+    {"name" : "Phil", "age" : 20, "sex": "Male"}, 
+    {"name": "John", "age" : 35, "sex": "Female"},
+    {"name" : "Jonathan", "age" : 25, "sex": "Female"}, 
+    {"name": "Sandra", "age" : 23, "sex": "Female"},
+    {"name" : "Jennifer", "age" : 20, "sex": "Male"},
+    {"name" : "Jessica", "age" : 20, "sex": "Male"},
+    {"name" : "Sara", "age" : 20, "sex": "Male"}]  
+   },
     specPattern:'cypress/e2e/*.js',
     
   },
