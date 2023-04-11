93bc5d64f7fcf26d5baf118dc02c49c24db6803e
draqulaboy1 <himanshu.kumar@cawstudios.com>
2023-04-12 01:55:35 +0530

    seconf commit

--- cypress/e2e/assigment 1.js
+++ cypress/e2e/assigment 1.js
@@ -1,38 +1,52 @@
 /// <reference types="cypress" />
+
 describe('My First Test', () => {
 
-
-    it('Verify the title page', () => {
+    before(function () {
+        cy.fixture('example').then(function (data) {
+          this.data = data
+        })
+      })
+    it('Verify the title page', function() {
      cy.visit('https://testpages.herokuapp.com/styled/tag/dynamic-table.html')
      cy.get('summary').click()
-     const s =   Cypress.env('json')
-     const string = JSON.stringify(s)
+    
+    
+    const string =JSON.stringify(this.data)
      
     
-     cy.get('textarea#jsondata').click().clear().type(string, { parseSpecialCharSequences: false })
+     cy.get('textarea#jsondata').click().clear().type(string,{ parseSpecialCharSequences: false })
      
      cy.get('#refreshtable').click()
-
-    var i;
-    for(i=0;i<=8;i++){
-    
-        cy.get('#dynamictable>tr>td').eq(i*3).should('contain',s[i]["name"])
-    }
-    
-    for(i=0;i<=8;i++){
-    
-        cy.get('#dynamictable>tr>td').eq(2*i+1*(i+1)).should('contain',s[i]["age"])
-    }
-
-    for(i=0;i<=8;i++){
-       
-            cy.get('#dynamictable>tr>td').eq(i*2+1*(i+2)).should('contain',s[i]["sex"])
-        }
+     
+    //  console.log(key)
+    //     cy.log(key[0])
+        //var i
+        
+            //var selector = '#dynamictable>tr>td:nth-child('+(i).toString()+')'
+            cy.get('#dynamictable>tr>td:nth-child(1)').each((el,index)=>{
+           
+               expect(el.text()).to.equal(this.data[index].name)
             
+            })
+        
+    cy.get('#dynamictable>tr>td:nth-child(2)').each((el,index,$list)=>{
+           
+        expect(el.text()).to.equal((this.data[index].age).toString())
+   
+})
+cy.get('#dynamictable>tr>td:nth-child(3)').each((el,index,$list)=>{
+           
+    expect(el.text()).to.equal(this.data[index].sex)
 
-
-
-
+})
+//}
+    
+           
+           
+                
+            
+    
 
 
 
